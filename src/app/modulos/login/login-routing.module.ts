bd9ae785f9d9a3980ddd1ccafa6cf6aedc120d65
Vanessa <wafer0210@outlook.com>
2023-12-01 01:48:54 -0500

    Actualización: agregados enrutamientos en module routing

--- src/app/modulos/login/login-routing.module.ts
+++ src/app/modulos/login/login-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+import { OlvidarClaveComponent } from './olvidar-clave/olvidar-clave.component';
+import { CerrarSesionComponent } from './cerrar-sesion/cerrar-sesion.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+import { VerificacionCodigoComponent } from './verificacion-codigo/verificacion-codigo.component';
 
 const routes: Routes = [
 
@@ -10,7 +15,26 @@ const routes: Routes = [
   },
   {
     path: "cerrar-sesion",
+    component: CerrarSesionComponent
+  },
+  {
+    path: "registrarse",
+    component: RegistrarseComponent
+  },
+  {
+    path: "olvidar-clave",
+    component: OlvidarClaveComponent
+  },
+  {
+    path: "recuperar-clave",
+    component: RecuperarClaveComponent
+  },
+  {
+    path: "verificacion-codigo",
+    component: VerificacionCodigoComponent
   }
+
+
 
 
 
