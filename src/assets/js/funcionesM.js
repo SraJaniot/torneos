8a671720a04a13de721d97ca8cae11a7be548e0f
Vanessa <wafer0210@outlook.com>
2023-12-01 02:35:18 -0500

    front inicio

---
+++ src/assets/js/funcionesM.js
@@ -0,0 +1,52 @@
+function cumpleanios(){
+
+      document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {
+
+    });
+  });
+  
+}
+
+
+cumpleanios();
+
+
+
+/*var app = angular.module('mainModule', []);
+
+app.controller('mainController', function($scope, $http){ //o scope liga o js e o template
+  $scope.nome = 'Valor Inicial';
+  //$http.get().success();
+  $scope.reset = function()
+  {
+    $scope.nome = '';
+  }
+});*/
+
+
+
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+/*window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+*/
+// Or with jQuery
+
